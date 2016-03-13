7e2bea9b5e0c20b7fa6925bff8faaaa7699bffd3
Tim Jones <tim@timjones.tw>
2016-03-13 18:57:23 +0800

    Remove unnecessary files

--- src/OnlineHlslCompiler/Controllers/HomeController.cs
+++ src/OnlineHlslCompiler/Controllers/HomeController.cs
@@ -19,19 +19,5 @@ namespace OnlineHlslCompiler.Controllers
 
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
