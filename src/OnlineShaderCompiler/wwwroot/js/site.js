05458e42faf4ed1afcb1a4d522e139e05237129c
Tim Jones <tim@timjones.io>
2018-05-03 17:57:41 +0800

    Initial commit of new site

---
+++ src/OnlineShaderCompiler/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    var codeEditor = CodeMirror(document.getElementById('code-editor-container'), {
+        value: $("#code-input").val(),
+        mode: "x-shader/hlsl",
+        theme: "neat",
+        lineNumbers: true,
+        matchBrackets: true,
+        styleActiveLine: true,
+        indentUnit: 4
+    });
+
+    var codeEditor = CodeMirror(document.getElementById('compiler-output-container'), {
+        mode: "x-shader/llvm-ir",
+        theme: "neat",
+        lineNumbers: true,
+        matchBrackets: true,
+        styleActiveLine: true,
+        indentUnit: 4,
+        readOnly: true
+    });
+});
