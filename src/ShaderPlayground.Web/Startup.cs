1dc70a772c3fe05d8fe3a418d6d424e872147ad0
Tim Jones <tim@timjones.io>
2019-01-20 21:23:15 +0000

    Update dependencies

--- src/ShaderPlayground.Web/Startup.cs
+++ src/ShaderPlayground.Web/Startup.cs
@@ -25,7 +25,6 @@ namespace ShaderPlayground.Web
         {
             if (env.IsDevelopment())
             {
-                app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
             }
 
